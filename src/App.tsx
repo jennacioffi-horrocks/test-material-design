49a94e75388787a975d46b282564fa0fa919c46a
jennacioffi-horrocks <jenna.cioffi@horrocks.com>
2024-10-01 12:25:18 -0600

    material design theme and app.tsx testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css';
+import './App.css'; 
 
-function App() {
-  const [count, setCount] = useState(0)
+// Material Design
+import '@material/web/button/filled-button.js';
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="container">
+      <h1>Material Web with TypeScript</h1>
 
-export default App
+      {/* Use the Material Web Button */}
+      <md-filled-button className="md-filled-button" label="Click Me">
+        Click Me
+      </md-filled-button>
+    </div>
+  );
+};
+
+export default App;
